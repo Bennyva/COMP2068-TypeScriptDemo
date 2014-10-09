252d81d77c9d72a7d400ed84d352b3103efa884d
tsiliopoulos <tsiliopoulos@hotmail.com>
2014-10-09 11:50:51 -0400

    created typescript structure based on Boilerplate template

---
+++ COMP2068-TypeScriptDemo/Scripts/app.ts
@@ -0,0 +1,15 @@
+﻿class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    moves(meters: number) {
+        alert(this.name + " moves " + meters + "m");
+    }
+}
+
+var tom = new Person("Tom", 30);
+tom.moves(5); 
